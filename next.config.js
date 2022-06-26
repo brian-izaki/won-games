d94d5e9acd9e02ae7c82ebc54a231a838ed6a709
brian-izaki <brian.izaki@gmail.com>
2022-06-26 16:50:05 -0300

    config next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPwa = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
+
+module.exports = withPwa({
+  pwa: {
+    // destino dos arquivos de service worker
+    dest: 'public',
+    disable: !isProd
+  }
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,9 @@ function App({ Component, pageProps }: AppProps) {
         <title>Curso de React Avançado - boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+
         <meta
           name="description"
           content="A simple project starter with Typescript, React, Next and styled components"
